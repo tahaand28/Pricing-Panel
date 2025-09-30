25846295726a2f3031b3b3f09e30e684bb31f49d
Mohamad taha Ahmadi <mohamad.taha.ahmadi2024@gmail.com>
2025-09-30 21:55:05 +0330

    pricing-panel
    This is a pricing panel built with HTML and CSS and is separate from the animated background.

---
+++ pricing-panel/script.js
@@ -0,0 +1,34 @@
+new FinisherHeader({
+    "count": 6,
+    "size": {
+        "min": 1100,
+        "max": 1300,
+        "pulse": 0
+    },
+    "speed": {
+        "x": {
+            "min": 0.1,
+            "max": 0.3
+        },
+        "y": {
+            "min": 0.1,
+            "max": 0.3
+        }
+    },
+    "colors": {
+        "background": "#ffffff",
+        "particles": [
+            "#7a7ef3",
+            "#9fa2ea"
+        ]
+    },
+    "blending": "overlay",
+    "opacity": {
+        "center": 1,
+        "edge": 0.1
+    },
+    "skew": 0,
+    "shapes": [
+        "c"
+    ]
+});
